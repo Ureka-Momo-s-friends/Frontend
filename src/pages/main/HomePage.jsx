676297d1b46550a086fc0cd97c1541ff95e5deea
unfamiliarsik <kes6534@naver.com>
2024-10-25 10:34:15 +0900

    login push

--- src/pages/main/HomePage.jsx
+++ src/pages/main/HomePage.jsx
@@ -1,9 +1,13 @@
 import React from "react";
+import Login from "../Login";
 
 function HomePage() {
   return (
     <div>
       <h1>Home Page</h1>
+      <section>
+        <Login></Login>
+      </section>
     </div>
   );
 }
