4152aac7473bed147a50d0322fe56ffca8372c2f
Jeongmin <srm0609@naver.com>
2024-11-01 15:07:51 +0900

    Update: ProductDetail

--- src/components/Product/ProductDetail/index.jsx
+++ src/components/Product/ProductDetail/index.jsx
@@ -30,29 +30,39 @@ const ProductDetail = () => {
   return (
     <S.Layer>
       <Header isBack={true} />
-      <S.Thumnail>
-        <img src={product.thumbnail} alt={product.name} width={"100%"} />
-      </S.Thumnail>
-      <S.Info>
-        <h5>{product.name}</h5>
-        <div>
-          <S.OriginalPrice>{product.price.toLocaleString()}원</S.OriginalPrice>
-          <span>
-            <S.DiscountRate>
-              {Math.round(
-                ((product.price - product.salePrice) / product.price) * 100,
-              )}
-              %
-            </S.DiscountRate>
-            <S.SalePrice>{product.salePrice.toLocaleString()}원</S.SalePrice>
-          </span>
-        </div>
-      </S.Info>
-      <S.Descripsion>
-        <h5>상품 설명</h5>
-        <div>{product.detail}</div>
-      </S.Descripsion>
-      <BuyAndCart productName={product.name} price={product.salePrice} />
+      {product ? (
+        <>
+          <S.Thumnail>
+            <img src={product.thumbnail} alt={product.name} width={"100%"} />
+          </S.Thumnail>
+          <S.Info>
+            <h5>{product.name}</h5>
+            <div>
+              <S.OriginalPrice>
+                {product.price.toLocaleString()}원
+              </S.OriginalPrice>
+              <span>
+                <S.DiscountRate>
+                  {Math.round(
+                    ((product.price - product.salePrice) / product.price) * 100,
+                  )}
+                  %
+                </S.DiscountRate>
+                <S.SalePrice>
+                  {product.salePrice.toLocaleString()}원
+                </S.SalePrice>
+              </span>
+            </div>
+          </S.Info>
+          <S.Descripsion>
+            <h5>상품 설명</h5>
+            <div>{product.detail}</div>
+          </S.Descripsion>
+          <BuyAndCart productName={product.name} price={product.salePrice} />
+        </>
+      ) : (
+        <div>로딩 중...</div>
+      )}
     </S.Layer>
   );
 };
