edd83009f876020bd54aca2f424c8a017a8d9020
hongsi0 <hongsizero@gmail.com>
2024-11-05 17:15:27 +0900

    Delete CartPage.jsx

--- src/pages/main/ProductSearchPage.jsx
+++ src/pages/main/ProductSearchPage.jsx
@@ -1,14 +1,13 @@
 import React from "react";
-import Layout from "components/Layout";
 import Search from "components/Product/Search";
 import SearchBox from "components/Main/SearchBox";
 
 function ProductSearchPage() {
   return (
-    <Layout>
+    <>
       <SearchBox />
       <Search />
-    </Layout>
+    </>
   );
 }
 
